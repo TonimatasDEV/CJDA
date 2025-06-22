5911e8b9281580451792d57928cc5c866a6e0a44
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-22 21:11:17 +0200

    More things

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,16 @@ dependencies {
     }
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
@@ -26,6 +36,37 @@ publishing {
             groupId = group.toString()
             artifactId = "CJDA"
             version = project.version.toString()
+
+            artifact(sourcesJar.get()) {
+                classifier = "sources"
+                extension = "jar"
+            }
+            artifact(javadocJar.get()) {
+                classifier = "javadoc"
+                extension = "jar"
+            }
+
+            pom {
+                name.set("CJDA")
+                description.set("Command Java Discord Api")
+                url.set("https://github.com/TonimatasDEV/CJDA")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/TonimatasDEV/CJDA/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("tonimatasdev")
+                        name.set("TonimatasDEV")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/TonimatasDEV/CJDA.git")
+                    url.set("https://github.com/TonimatasDEV/CJDA")
+                }
+            }
         }
     }
 
